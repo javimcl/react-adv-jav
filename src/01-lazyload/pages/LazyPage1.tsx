88cf6c321ebc870d6c035b01d283a591f22fa9b2
Javier Lucero <javier.lucero@iess.gob.ec>
2022-03-22 16:29:48 -0500

    first commit

---
+++ src/01-lazyload/pages/LazyPage1.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const LazyPage1 = () => {
+  return (
+    <div>
+        <h1>LazyPage1</h1>
+        
+        
+    </div>
+  )
+}
+
+export default LazyPage1;
