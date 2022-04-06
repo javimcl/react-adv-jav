d60c795d205dac5ef4f57f85710abc47c34977f0
Javier Lucero <javier.lucero@iess.gob.ec>
2022-04-06 16:19:34 -0500

    finalizacion

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -27,7 +27,7 @@ export const ShoppingPage = () => {
             <ProductCard
                 key={product.id}
                 product={product}
-                className="bg-dark text-white"
+                
                 initialValues={{
                     count: 4,
                     maxCount: 10
@@ -37,20 +37,11 @@ export const ShoppingPage = () => {
                 {
                     ({reset, isMaxCountReached, count, increaseBy, maxCount}) => (
                         <>
-                            <ProductoImage className="custom-image" style={{
-                                boxShadow: '10px 10px 10px rgba(0,0,0,0.2)'
-                            }} />
-                            <ProductoTitle className=" text-bold" activeClass='active' />
-                            <ProductButtons className="custom-buttons" />
+                            <ProductoImage />
+                            <ProductoTitle  />
+                            <ProductButtons />
 
-                            <button onClick={reset}> Reset</button>
-                            <button onClick={() => increaseBy(-2)}>-2</button>
-                            {
-                                (!isMaxCountReached && <button onClick={() => increaseBy(2)}>+2</button>                            )
-                            }
-                            
-                            
-                            <span>{count} - {maxCount}</span>
+                           
                             
 
                         </>
